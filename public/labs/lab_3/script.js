12f903da5c2b27e6e4f13fdbc0c79dca45e6436d
mhough1 <mhough113@gmail.com>
2021-02-12 18:43:13 -0600

    lab 3

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,30 @@
 /* Put your javascript in here */
+"use strict";
+
+let width = 130;
+let count = 3;
+
+let list = carousel.querySelector("ul");
+let listElems = carousel.querySelectorAll("li");
+
+let position = 0; // scroll position
+
+function arrowPrev() 
+{
+  // shift left
+  position += width * count;
+  // clamp position
+  position = Math.min(position, 0);
+  // transform
+  list.style = "Transform: translateX(" + position + "px)";
+}
+
+function arrowNext() 
+{
+  // shift right
+  position -= width * count;
+  // clamp position
+  position = Math.max(position, -width * (listElems.length - count));
+  // transform
+  list.style = "Transform: translateX(" + position + "px)";
+}
